6db9ab6ca699da9bf934d0dbd61905b3f953697a
rizkivmaster <rizkivmaster@gmail.com>
2015-05-05 10:52:07 +0700

    add new accessors

---
+++ ion-central/src/main/java/org/ion/client/domain/Invoice.java
@@ -0,0 +1,15 @@
+package org.ion.client.domain;
+
+/**
+ * // TODO Comment
+ */
+public class Invoice {
+  private long id;
+  private String subject;
+  private String description;
+  private long amount;
+  private boolean hasPaid;
+  private String usernameCreator;
+  private String usernameTarget;
+
+}
